b8441d9e30dcec99aba55e96a9f50545b75b384b
rbroberg <mu@cow.net>
2014-09-08 18:55:10 -0600

    train to training

--- afsis.soil/scripts/africa.py
+++ afsis.soil/scripts/africa.py
@@ -12,7 +12,7 @@ headersTest = np.genfromtxt('../data/sorted_test.csv', delimiter=',', names=True
 #Columns to be picked from training file
 pickTrain = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI','Depth','Ca','P','pH','SOC','Sand']
 
-data = np.genfromtxt('../data/train.csv', names=True, delimiter=',') #, usecols=(pickTrain))
+data = np.genfromtxt('../data/training.csv', names=True, delimiter=',') #, usecols=(pickTrain))
 
 #Column to be picked from test file
 pickTest = ['PIDN', 'BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
