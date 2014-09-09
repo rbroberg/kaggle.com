913216ff3c34f5cb7514766837ffa060ef262e3d
rbroberg <mu@cow.net>
2014-09-08 18:54:18 -0600

    updated script from hiransuvrat

--- afsis.soil/scripts/africa.py
+++ afsis.soil/scripts/africa.py
@@ -2,44 +2,61 @@ import numpy as np
 import utilities as util
 import sklearn.linear_model as linear
 import sklearn.ensemble as ensemble
-#from sklearn.linear_model import BayesianRidge
 from sklearn import cross_validation
 import pandas as pd
 import sys
+import sklearn.feature_selection as fs
+
+headersTest = np.genfromtxt('../data/sorted_test.csv', delimiter=',', names=True)
 
 #Columns to be picked from training file
 pickTrain = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI','Depth','Ca','P','pH','SOC','Sand']
-data = np.genfromtxt("../data/training.csv", names=True, delimiter=',', usecols=(pickTrain))
+
+data = np.genfromtxt('../data/train.csv', names=True, delimiter=',') #, usecols=(pickTrain))
 
 #Column to be picked from test file
 pickTest = ['PIDN', 'BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
-test = np.genfromtxt("../data/sorted_test.csv", names=True, delimiter=',', usecols=(pickTest))
+test = np.genfromtxt('../data/sorted_test.csv', names=True, delimiter=',')#, usecols=(pickTest))
 
-ids = np.genfromtxt("../data/sorted_test.csv", dtype=str, skip_header=1, delimiter=',', usecols=0)
+ids = np.genfromtxt('../data/sorted_test.csv', dtype=str, skip_header=1, delimiter=',', usecols=0)
 
 #Features to train model on
-featuresList = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
+#featuresList = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
+featuresList = np.array(headersTest.dtype.names)
+featuresList = featuresList[2:-5]
 
 #Keep a copy of train file for later use
 data1 = np.copy(data)
 
 #Dependent/Target variables
 targets = ['Ca','P','pH','SOC','Sand']
+#targets = ['P']
 
 #Prepare empty result
 df = pd.DataFrame({"PIDN": ids, "Ca": test['PIDN'], "P": test['PIDN'], "pH": test['PIDN'], "SOC": test['PIDN'], "Sand": test['PIDN']})
 
 for target in targets:
     #Prepare data for training
-    data, testa, features, fillVal = util.prepDataTrain(data1, target, featuresList, False, 10, False, True, 'mean', False,'set')
-    
-    print 'Data prepped'
-    
+    data = np.copy(data1)
+    delList = np.array([])
+    for i in range(len(data)):
+        if data[target][i] > (data[target].mean() + 2*data[target].std()) or data[target][i] < (data[target].mean() - 2*data[target].std()):
+            delList = np.append(delList, i)
+    data = np.delete(data, delList, 0)
+    data, testa, features, fillVal = util.prepDataTrain(data, target, featuresList, False, 10, False, True, 'mean', False, 'set')
+    #sel = fs.SelectKBest(fs.f_regression, k=2000)
+    print data.shape
+    #data = np.array(sel.fit_transform(data[features].tolist(), data1[target]))
+    #print data.shape
     #Use/tune your predictor
-    clf = ensemble.GradientBoostingRegressor(n_estimators=20)
-    #clf = BayesianRidge(compute_score=True)
-    clf.fit(data[features].tolist(), data[target])
+    clf = linear.BayesianRidge(normalize=True, verbose=True, tol=.01)
     
+    #scores = cross_validation.cross_val_score(clf, data[features].tolist(), data[target], cv=5, scoring='mean_squared_error')
+    scores = np.array(cross_validation.cross_val_score(clf, data[features].tolist(), data[target], cv=5, scoring='mean_squared_error'))
+    print (-1 * scores), (-1  * scores.sum()/5)
+    continue
+    clf.fit(data[features].tolist(), data[target])
+
     #Prepare test data
     test = util.prepDataTest(test, featuresList, True, fillVal, False, 'set')
     
@@ -49,6 +66,7 @@ for target in targets:
     #Store results
     df[target] = pred
 
-df.to_csv("../submissions/submit.africa.GradientBoostingRegressor.csv", index=False, cols=["PIDN","Ca","P","pH","SOC","Sand"])
+df.to_csv("../submissions.bayesianridge.params.csv", index=False, cols=["PIDN","Ca","P","pH","SOC","Sand"])
+
 
 
