83c310c3b559e692c0e03b15c7af0d002f8d8d66
rbroberg <mu@cow.net>
2014-09-08 20:05:39 -0600

    panda-ized

--- afsis.soil/scripts/africa.py
+++ afsis.soil/scripts/africa.py
@@ -8,25 +8,21 @@ import sys
 import sklearn.feature_selection as fs
 
 headersTest = np.genfromtxt('../data/sorted_test.csv', delimiter=',', names=True)
+dftest = pd.io.parsers.read_csv('../data/sorted_test.csv')
+dftest = # 727, 3595
+dftraining = pd.io.parsers.read_csv('../data/training.csv')
+dftraining.shape # 1157, 3600
 
-#Columns to be picked from training file
-pickTrain = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI','Depth','Ca','P','pH','SOC','Sand']
+# extract labels (Y) and training data (X) (without sample id hashes)
+dflabels = dftraining.iloc[:,3595:3600]
+dftrain = dftraining.iloc[:,1:3595]
+dftrain.shape # 1157, 3594
 
-data = np.genfromtxt('../data/training.csv', names=True, delimiter=',') #, usecols=(pickTrain))
+# binary categories for Topsoil -v- Subsoil
+dftrain.iloc[0,3593] # Topsoil
+dftrain['Depth'][dftrain['Depth']=='Topsoil']=1
+dftrain['Depth'][dftrain['Depth']=='Subsoil']=0
 
-#Column to be picked from test file
-pickTest = ['PIDN', 'BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
-test = np.genfromtxt('../data/sorted_test.csv', names=True, delimiter=',')#, usecols=(pickTest))
-
-ids = np.genfromtxt('../data/sorted_test.csv', dtype=str, skip_header=1, delimiter=',', usecols=0)
-
-#Features to train model on
-#featuresList = ['BSAN','BSAS','BSAV','CTI','ELEV','EVI','LSTD','LSTN','REF1','REF2','REF3','REF7','RELI','TMAP','TMFI']
-featuresList = np.array(headersTest.dtype.names)
-featuresList = featuresList[2:-5]
-
-#Keep a copy of train file for later use
-data1 = np.copy(data)
 
 #Dependent/Target variables
 targets = ['Ca','P','pH','SOC','Sand']
@@ -36,37 +32,23 @@ targets = ['Ca','P','pH','SOC','Sand']
 df = pd.DataFrame({"PIDN": ids, "Ca": test['PIDN'], "P": test['PIDN'], "pH": test['PIDN'], "SOC": test['PIDN'], "Sand": test['PIDN']})
 
 for target in targets:
-    #Prepare data for training
-    data = np.copy(data1)
-    delList = np.array([])
-    for i in range(len(data)):
-        if data[target][i] > (data[target].mean() + 2*data[target].std()) or data[target][i] < (data[target].mean() - 2*data[target].std()):
-            delList = np.append(delList, i)
-    data = np.delete(data, delList, 0)
-    data, testa, features, fillVal = util.prepDataTrain(data, target, featuresList, False, 10, False, True, 'mean', False, 'set')
-    #sel = fs.SelectKBest(fs.f_regression, k=2000)
-    print data.shape
-    #data = np.array(sel.fit_transform(data[features].tolist(), data1[target]))
-    #print data.shape
-    #Use/tune your predictor
+dflabels[target] < dflabels[target].std() + 2*dflabels[target].std()
+    # filter outliers (the logic is reversed, True is NOT OUTLIER)
+    outliers = (dflabels[target] < dflabels[target].std() + 2*dflabels[target].std()) & (dflabels[target] > dflabels[target].std() - 2*dflabels[target].std())
+    sum(outliers) # 1131 for 'Ca'
+    
     clf = linear.BayesianRidge(normalize=True, verbose=True, tol=.01)
     
-    #scores = cross_validation.cross_val_score(clf, data[features].tolist(), data[target], cv=5, scoring='mean_squared_error')
-    scores = np.array(cross_validation.cross_val_score(clf, data[features].tolist(), data[target], cv=5, scoring='mean_squared_error'))
+    scores = np.array(cross_validation.cross_val_score(clf, dftrain[outliers], dflabels[target][outliers], cv=5))
     print (-1 * scores), (-1  * scores.sum()/5)
     continue
-    clf.fit(data[features].tolist(), data[target])
+    clf.fit(dftrain[outliers], dflabels[target][outliers])
 
-    #Prepare test data
-    test = util.prepDataTest(test, featuresList, True, fillVal, False, 'set')
-    
     #Get predictions
-    pred = clf.predict(test[features].tolist())
+    pred = clf.predict(dftest.tolist())
     
     #Store results
     df[target] = pred
 
 df.to_csv("../submissions.bayesianridge.params.csv", index=False, cols=["PIDN","Ca","P","pH","SOC","Sand"])
-
-
 
